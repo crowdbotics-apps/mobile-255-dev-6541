433ed0018bf1d188c9311458fcc400a0cd16b31a
Crowdbotics <team@crowdbotics.com>
2020-06-25 07:48:41 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,7 +3,6 @@ export const installed_blueprints = [
 { name: 'Messaging6699', human_name: 'Messaging', access_route: 'Messaging6699', icon: 'comments'},
 { name: 'EmailAuth6698', human_name: 'Email Auth', access_route: 'EmailAuth6698', icon: 'envelope-o'},
 { name: 'CalendarView6697', human_name: 'Calendar View', access_route: 'CalendarView6697', icon: 'calendar'},
-{ name: 'BlankScreen46696', human_name: 'Blank Screen 4', access_route: 'BlankScreen46696'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Calendar', human_name: 'Calendar', access_route: 'Calendar', icon: 'calendar'},
 { name: 'EmailAuth', human_name: 'EmailAuth', access_route: 'EmailAuth', icon: 'envelope-o'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import Messaging6699Navigator from '../features/Messaging6699/navigator';
 import EmailAuth6698Navigator from '../features/EmailAuth6698/navigator';
 import CalendarView6697Navigator from '../features/CalendarView6697/navigator';
-import BlankScreen46696Navigator from '../features/BlankScreen46696/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import CalendarNavigator from '../features/Calendar/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
@@ -25,7 +24,6 @@ const AppNavigator = {
 Messaging6699: { screen: Messaging6699Navigator },
 EmailAuth6698: { screen: EmailAuth6698Navigator },
 CalendarView6697: { screen: CalendarView6697Navigator },
-BlankScreen46696: { screen: BlankScreen46696Navigator },
 Messenger: { screen: MessengerNavigator },
 Calendar: { screen: CalendarNavigator },
 EmailAuth: { screen: EmailAuthNavigator },
